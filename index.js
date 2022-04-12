26897aea19d59b4ab74fe1c1aad9f34119f12962
BobbyDeluna <bobby.deluna2@gmail.com>
2022-04-12 14:33:36 -0700

    commit tree 74e4af8bb0e704f0af8704bda1f2d25f8ca8c2f4

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const nameData = {
+    name: "Steve",
+    email: "steve@steve.com",  
+};
+const message = ("Unauthorized Access");
+
+function submitData(){
+const blam = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(nameData)        
+    }
+
+return fetch("http://localhost:3000/users", blam)
+.then(res => res.json())
+.then(body => addNewContact(body.id))
+.catch(response => "Unauthorized Access");
+
+function addNewContact(newID){
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+    document.body.append(message)
+}}
